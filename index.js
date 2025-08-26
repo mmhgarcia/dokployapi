0ab22c093fbf737bee48a59552ce40e1af1272c8
Alberto Garcia <mmhgarcia@gmail.com>
2025-08-26 08:53:57 -0400

    Cambio puerrto del server de 3000 a 3030.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
   res.send('¡Hola desde Express en Dokploy!');
